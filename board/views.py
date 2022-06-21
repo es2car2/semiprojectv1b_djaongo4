b5fcf759a57be25e491887bdb84aba1b8458ac60
LSW <692622@gmail.com>
2022-06-21 12:37:47 +0900

    django app의 home,board,member의 urls/views 생성/수정

--- board/views.py
+++ board/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def list(request):
+    return render(request, 'board/list.html')
+                    # request , 파일명(위치)
+
+
+def view(request):
+    return render(request, 'board/view.html')
+
+
+def write(request):
+    return render(request, 'board/write.html')
